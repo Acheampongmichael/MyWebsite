cb0d831cda6739efd0ab8183baaa33b36951b9c2
Acheampongmichael <80366178+Acheampongmichael@users.noreply.github.com>
2024-03-14 22:42:22 -0400

    Read more changes

--- index.js
+++ index.js
@@ -2,27 +2,14 @@ let sunElem = document.querySelector('.sun')
 let bodyElem = document.querySelector('body')
 let picElem = document.querySelector('.profile-pic')
 
-
-sunElem.addEventListener('click',function(){
+sunElem.addEventListener('click', function() {
     bodyElem.classList.toggle('dark-theme')
-    if (bodyElem.classList.contains('dark-theme')){
+    if (bodyElem.classList.contains('dark-theme')) {
         sunElem.src = 'images/moon.png'
-    }
-    else{
+    } else {
         sunElem.src = 'images/sun.png'
     }
 })
-
-
-function toggleVisibility(id) {
-    var element = document.getElementById(id);
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
-}
-
 
 // Function to toggle fade in and out effect
 function toggleFadeInOut() {
@@ -35,14 +22,10 @@ window.onload = function() {
     setInterval(toggleFadeInOut, 4000); // Toggle every 4 seconds (2s fadeIn + 2s fadeOut)
 }
 
-
 // Function to toggle visibility of additional content
 function toggleVisibility(elementId) {
     var element = document.getElementById(elementId);
     element.style.display === "none" ? element.style.display = "block" : element.style.display = "none";
 }
-
-
-
 
 
